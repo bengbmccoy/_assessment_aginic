cc78dc419a0088c86a5d3c6bccf6b40cafd8974b
bengbmccoy <benjamingbmccoy@gmail.com>
2020-02-08 16:55:18 +1100

    a few major changes to get down before I watch the game with Dad. I have simplified the initial plan that I had developed and no longer require as many steps before I have the templates of each activities data nested dictionaries. Now the script acts as follows: 1. parse the command line arguments, 2. load the sample JSON file, 3. extract the keys for the metadata fields, 4. for each activities data sub-field (note or shipping) make a template and empty the values that need to be randomized, 5. iterate through the amount of activites required to generate and randomly choose an activity type and then fill it using randomly generated data, 6. piece the new activities dicts into a new JSON, 7. Fill the meta data, 8. write to disk.  I still need to finish steps 5 onwards.

--- ticket_generator.py
+++ ticket_generator.py
@@ -21,6 +21,8 @@ datetimes.
 
 import argparse
 import json
+import datetime
+import random
 
 def main():
 
@@ -39,37 +41,98 @@ def main():
         sample_json = json.load(read_file)
 
     if args.verbose:
-        print(json.dumps(sample_json, indent=4))
-        print('')
+        print('the sample JSON looks like below:')
+        print(json.dumps(sample_json, indent=4), '\n')
 
     meta_fields = sample_json['metadata'].keys()
-    print('metadata fields collected')
-    print(meta_fields, '\n')
+    if args.verbose:
+        print('metadata fields collected')
+        print(meta_fields, '\n')
 
-    activity_fields = sample_json['activities_data'][0].keys()
-    print('activity fields collected')
-    print(activity_fields, '\n')
-
-    empty_activities = []
+    activities_samples = []
     for i in range(sample_json['metadata']['activities_count']):
-        empty_activities.append(empty_dict(sample_json['activities_data'][i]['activity']))
-    empty_activities = remove_duplicates(empty_activities)
-    print('empty activity fields collected')
-    print(empty_activities, '\n')
+            activities_samples.append(sample_json['activities_data'][i])
+    activities_samples = remove_duplicates(activities_samples)
+    # print(activities_samples)
+
+    # empty_act_json = empty_dict(activities_samples[0])
+    empty_act_json = empty_dict(activities_samples[1])
+
+    new_activity = fill_empty_act_json(empty_act_json)
+    print(new_activity)
+
+def fill_empty_act_json(json):
+
+    for k, v in json.items():
+        if isinstance(v, dict):
+            fill_empty_act_json(v)
+        else:
+            if v == '':
+                json[k] = gen_value(k)
+    return json
+
+def gen_value(k):
+    '''Takes a key from the json and returns a value that is randomly generated
+    and matches the data from that key. Some keys will require the use of more
+    complex functions to generate random values/datetimes.'''
+
+    if k in ['performed_at', 'start_at', 'end_at']:
+        val = gen_rand_datetime()
+    elif k in ['ticket_id']:
+        val = random.randint(100,999)
+    elif k in ['performer_id', 'agent_id', 'requester']:
+        val = random.randint(100000, 999999)
+    elif k in ['id']:
+        val = random.randint(1000000, 9999999)
+    elif k in ['shipment_date']:
+        val = 'DD MMM YYYY'
+    elif k in ['status']:
+        val = gen_rand_status()
+
+    return val
+
+def gen_rand_status():
+    '''returns a random value out of Open, Closed, Resolved, Waiting for
+    Customer, Waiting for Third Party, or Pending'''
+
+    status_vals = ['Open', 'Closed', 'Resolved', 'Waiting for Customer',
+    'Waiting for Third Party', 'Pending']
+
+    return status_vals[random.randint(0,5)]
+
+def gen_rand_datetime():
+    '''generates a random datetime between the date provided in the sample JSON
+    and the current date of writing this script. Removes the decimals on the
+    seconds, adds the timezone afterwards'''
+
+    start = '20-04-2017 10:00:00'
+    end = '20-04-2021 10:00:00'
+    frmt = '%d-%m-%Y %H:%M:%S'
+    stime = datetime.datetime.strptime(start, frmt)
+    etime = datetime.datetime.strptime(end, frmt)
+    td = etime - stime
+    print(random.random() * td + stime)
+    return str(random.random() * td + stime).split('.')[0] + ' +0000'
 
 def empty_dict(d):
-    '''Takes a nested python dict and sets all value to empty.'''
+    '''Takes a nested python dict and sets values as empty if they are in
+    'del fields'. '''
+
+    del_fields = ['performed_at', 'ticket_id', 'performer_id', 'id',
+    'shipment_date', 'status', 'agent_id', 'requester']
     for k, v in d.items():
         if isinstance(v, dict):
             empty_dict(v)
         else:
-            d[k] = ''
+            if k in del_fields:
+                d[k] = ''
     return d
 
 def remove_duplicates(l):
     '''Takes a list and returns the unique value in the list
     this function was required as nested python dicts are not hashable and thus
-    the function 'set' cannot be used on them.'''
+    the function 'set' cannot be used on them. '''
+
     output = []
     for item in l:
         if item not in output:
