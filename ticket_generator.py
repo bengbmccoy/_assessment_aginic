b9e122e82981778f4df8f179516a7809055c5977
bengbmccoy <benjamingbmccoy@gmail.com>
2020-02-08 11:47:35 +1100

    made significant headway on the ticket_generator.py script. I now have the skeleton of the json files and will be able to simply fill as many activity dictionaries as is required once I have built the class object that manages the json processing. Additionally, I have created the command line arguments and now have a good understanding of how the JSON files are structured. Time to go to the gym and see Dad.

--- ticket_generator.py
+++ ticket_generator.py
@@ -27,10 +27,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('sample_data', type=str,
                         help='sample JSON filepath')
+    parser.add_argument('-ticket_gen', type=int, default=100,
+                        help='number of tickets to generate, defualt is 100')
     parser.add_argument('-output_file', type=str,
                         help='name of output JSON file')
-    parser.add_argument('-v', '--verbose',
-                        action='store_true',
+    parser.add_argument('-v', '--verbose', action='store_true',
                         help='prints progress of script')
     args = parser.parse_args()
 
@@ -39,8 +40,41 @@ def main():
 
     if args.verbose:
         print(json.dumps(sample_json, indent=4))
+        print('')
 
-    
+    meta_fields = sample_json['metadata'].keys()
+    print('metadata fields collected')
+    print(meta_fields, '\n')
+
+    activity_fields = sample_json['activities_data'][0].keys()
+    print('activity fields collected')
+    print(activity_fields, '\n')
+
+    empty_activities = []
+    for i in range(sample_json['metadata']['activities_count']):
+        empty_activities.append(empty_dict(sample_json['activities_data'][i]['activity']))
+    empty_activities = remove_duplicates(empty_activities)
+    print('empty activity fields collected')
+    print(empty_activities, '\n')
+
+def empty_dict(d):
+    '''Takes a nested python dict and sets all value to empty.'''
+    for k, v in d.items():
+        if isinstance(v, dict):
+            empty_dict(v)
+        else:
+            d[k] = ''
+    return d
+
+def remove_duplicates(l):
+    '''Takes a list and returns the unique value in the list
+    this function was required as nested python dicts are not hashable and thus
+    the function 'set' cannot be used on them.'''
+    output = []
+    for item in l:
+        if item not in output:
+            output.append(item)
+    return output
 
 if __name__ == "__main__":
     main()
